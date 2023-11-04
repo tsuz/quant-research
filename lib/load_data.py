26b6060559effd132e55c970a0a058ea8d770dfc
Taku Suzuki <tak215@gmail.com>
2023-11-04 21:29:48 +0900

    add load data lib

---
+++ lib/load_data.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from lib.format import format_df
+import pandas as pd
+
+def load_data(symbol: str, start: str, end: str):
+    '''
+    Loads data from store
+    
+        Parameters:
+            symbol (str): symbol to load. (i.e. USDJPY)
+            start (str): start time to filter from in format of '1970-01-01 01:00:00:
+            end (str): end time to filter from in format of '1970-01-01 01:00:00:
+        
+        Returns:
+            out (pd.DataFrame): dataframe with loaded data containing OHLC. 
+                There may be additioanl attributes depending on the type of data.
+    '''
+
+    path = os.path.dirname(sys.modules['__main__'].__file__)
+
+    cached_path = f'{path}/data/{symbol}_{start}_{end}_formatted.csv'
+    data_path = f'{path}/data/{symbol}.txt'
+
+    if os.path.exists(cached_path):
+        df = pd.read_csv(cached_path)
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df = df.set_index('datetime')
+        df = df.sort_index()
+    else:
+        df = pd.read_csv(data_path)
+        df = format_df(df, 'forextester.com')
+
+    # Filter timeframe
+    df = df.loc[(df.index >= start) & (df.index <= end)]
+    
+    # Save to cache
+    df.to_csv(cached_path)
+
+    return df
+
