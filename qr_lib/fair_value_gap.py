70518241038e3e83074d8d850df9a537a1da55c0
Taku Suzuki <tak215@gmail.com>
2023-11-25 08:38:35 +0900

    add where fvg occurred

--- qr_lib/fair_value_gap.py
+++ qr_lib/fair_value_gap.py
@@ -18,7 +18,7 @@ def append_fvg(df: pd.DataFrame, fvg_min: float = 0, low_col: str = 'low', high_
                 - prev_fvg_high (number or NaN): The higher price of the most recent fair value gap
                 - prev_fvg_low (number or NaN): The lower price of the most recent fair value gap
     '''
-
+    fvg_occurred = 'fvg_occurred'
     prev_fvg = 'prev_fvg_bullish'
     prev_fvg_high = 'prev_fvg_high'
     prev_fvg_low = 'prev_fvg_low'
@@ -26,8 +26,11 @@ def append_fvg(df: pd.DataFrame, fvg_min: float = 0, low_col: str = 'low', high_
     bullish_fvg = (df[low_col] > df[high_col].shift(2) + fvg_min)
     bearish_fvg = (df[high_col] + fvg_min < df[low_col].shift(2))
 
+    df[fvg_occurred] = False
     df.loc[bullish_fvg, prev_fvg] = True
     df.loc[bearish_fvg, prev_fvg] = False
+
+    df.loc[(bullish_fvg | bearish_fvg), fvg_occurred] = True
 
     df.loc[df[prev_fvg] == True, prev_fvg_high] = df[low_col]
     df.loc[df[prev_fvg] == True, prev_fvg_low] = df[high_col].shift(2)
